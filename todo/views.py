c9e96e22be1655bc167ce7e03b354dbe98111cc4
Behailu Sileshi <behailusileshi000@gmail.com>
2024-12-30 17:00:35 +0300

    Create TaskViewSet and CategoryViewSet.

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from todo.models import Task
+from todo.serializers import CategorySerializer, TaskSerializer
 
 # Create your views here.
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    
+class CategoryViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = CategorySerializer
+    
